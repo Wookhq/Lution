202c95a7477c9a866e279ff3d8cd9a37942b78dd
evil chipo <itsjustchipoverhere@proton.me>
2025-12-18 18:11:25 +0700

    build a tray system

--- src/main.py
+++ src/main.py
@@ -1,13 +1,19 @@
-import sys, os
+import sys, os, webbrowser
 import darkdetect  # installed in rinui
 from pathlib import Path
+import subprocess
+from PySide6.QtWidgets import QSystemTrayIcon, QMenu
+from PySide6.QtGui import QIcon, QAction
 from PySide6.QtWidgets import QApplication
 from PySide6.QtCore import Signal, QObject, Slot
 from PySide6.QtGui import QGuiApplication
+from PySide6.QtQml import QQmlApplicationEngine
 from RinUI import RinUIWindow
+import resources_rc
 
 SCRIPT_DIR = Path(__file__).parent
 __version__ = "0.1.0"
+
 
 # rcs = Resource(SCRIPT_DIR=SCRIPT_DIR)  # new method
 
@@ -25,8 +31,50 @@ class AppInit(RinUIWindow):
 
         # title
         self.setProperty("title", "LutionRT")
+        
+        QApplication.instance().setQuitOnLastWindowClosed(False)
+
+        self.initTray()
+
+    def initTray(self):
+        icon = QIcon.fromTheme(
+            "application-x-executable",
+        )
 
 
+        self.tray = QSystemTrayIcon(icon, QApplication.instance())
+
+        menu = QMenu()
+
+        about = QAction("LutionRT", QApplication.instance())
+        show_action = QAction("Show", QApplication.instance())
+        quit_action = QAction("Quit", QApplication.instance())
+
+
+
+        show_action.triggered.connect(self.show)
+        quit_action.triggered.connect(QApplication.quit)
+
+        menu.addAction(about)
+        menu.addAction(show_action)
+        menu.addAction(quit_action)
+
+        self.tray.setContextMenu(menu)
+        self.tray.show()
+
+        self.tray.activated.connect(self.onTrayClick)
+
+
+    def onTrayClick(self, reason):
+        if reason == QSystemTrayIcon.Trigger:
+            self.show()
+            self.raise_()
+            self.activateWindow()
+
+
+    def closeEvent(self, event):
+        event.ignore()
+        self.hide()
 
 
 
@@ -35,7 +83,7 @@ class Backend(QObject):
 
     def __init__(self, parent=None):
         super().__init__(parent)
-        # self.rpc = VimalPresence("1442032843290181703")
+        # self.rpc = VimalPresence("1442032843290181703") as you can tell this code is from vimal
         # self.resc = rcs
 
     # @Slot(str, str)
@@ -66,6 +114,17 @@ class Backend(QObject):
     @Slot(str, result=str)
     def findResource(self, resource):
         return self.resc.find(resource)
+    
+    @Slot(str)
+    def openInBroswer(self, url):
+        webbrowser.open(url=url)
+
+    @Slot()
+    def openModFolder(self):
+        modpath = Path("~/.var/app/org.vinegarhq.Sober/data/sober/asset_overlay").expanduser()
+        subprocess.Popen(["xdg-open", str(modpath)])
+
+
 
 
 if __name__ == "__main__":
