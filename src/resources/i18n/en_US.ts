70d62ef16ed0732923d594be268e968188c897ea
evil chipo <itsjustchipoverhere@proton.me>
2025-12-25 22:16:21 +0700

    translation

--- src/resources/i18n/en_US.ts
+++ src/resources/i18n/en_US.ts
@@ -2,31 +2,85 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="en_US" sourcelanguage="en">
 <context>
+    <name>Calling</name>
+    <message>
+        <location filename="../ui/DefaultSplash/Calling.qml" line="129"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Default</name>
+    <message>
+        <location filename="../ui/DefaultSplash/Default.qml" line="116"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
     <name>Home</name>
     <message>
+        <location filename="../ui/pages/Home.qml" line="16"/>
         <source>Welcome back!</source>
-        <translation type="obsolete">Welcome back!</translation>
+        <translation type="unfinished">Welcome back!</translation>
+    </message>
+</context>
+<context>
+    <name>Integration</name>
+    <message>
+        <location filename="../ui/pages/Integration.qml" line="19"/>
+        <source>Custom Launch Splash</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pages/Integration.qml" line="20"/>
+        <source>Create or use your own custom Splash when launching sober with Chroma! Notice that this will not work with bloxstrap one.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pages/Integration.qml" line="24"/>
+        <source>Set Custom Launch Splash</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pages/Integration.qml" line="46"/>
+        <source>See how to make one</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pages/Integration.qml" line="49"/>
+        <source>Click here</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
     <name>ItemCard</name>
     <message>
+        <location filename="../ui/components/marketplace/ItemCard.qml" line="13"/>
         <source>Example Mod</source>
-        <translation type="obsolete">Example Mod</translation>
+        <translation type="unfinished">Example Mod</translation>
     </message>
     <message>
+        <location filename="../ui/components/marketplace/ItemCard.qml" line="14"/>
         <source>Placeholder text</source>
-        <translation type="obsolete">Placeholder text</translation>
+        <translation type="unfinished">Placeholder text</translation>
+    </message>
+    <message>
+        <location filename="../ui/components/marketplace/ItemCard.qml" line="15"/>
+        <location filename="../ui/components/marketplace/ItemCard.qml" line="16"/>
+        <source>Unkown</source>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <source>Unknown</source>
         <translation type="obsolete">Unknown</translation>
     </message>
     <message>
+        <location filename="../ui/components/marketplace/ItemCard.qml" line="118"/>
         <source>Description: %1
 Creator: %2
 %3</source>
-        <translation type="obsolete">Description: %1
+        <translation type="unfinished">Description: %1
 Creator: %2
 %3</translation>
     </message>
@@ -34,38 +88,44 @@ Creator: %2
 <context>
     <name>MainWindow</name>
     <message>
+        <location filename="../ui/MainWindow.qml" line="21"/>
         <source>Home</source>
-        <translation type="obsolete">Home</translation>
+        <translation type="unfinished">Home</translation>
     </message>
     <message>
+        <location filename="../ui/MainWindow.qml" line="26"/>
         <source>Marketplace</source>
-        <translation type="obsolete">Marketplace</translation>
+        <translation type="unfinished">Marketplace</translation>
     </message>
     <message>
+        <location filename="../ui/MainWindow.qml" line="31"/>
         <source>Mods</source>
-        <translation type="obsolete">Mods</translation>
+        <translation type="unfinished">Mods</translation>
     </message>
     <message>
+        <location filename="../ui/MainWindow.qml" line="36"/>
         <source>Fast flags</source>
-        <translation type="obsolete">Fast flags</translation>
+        <translation type="unfinished">Fast flags</translation>
     </message>
     <message>
+        <location filename="../ui/MainWindow.qml" line="40"/>
         <source>Integration</source>
-        <translation type="obsolete">Integration</translation>
+        <translation type="unfinished">Integration</translation>
     </message>
     <message>
+        <location filename="../ui/MainWindow.qml" line="45"/>
         <source>Settings</source>
-        <translation type="obsolete">Settings</translation>
+        <translation type="unfinished">Settings</translation>
     </message>
 </context>
 <context>
     <name>Marketplace</name>
     <message>
+        <location filename="../ui/pages/Marketplace.qml" line="10"/>
         <source>We&apos;re working on this one!</source>
-        <translation type="obsolete">We&apos;re working on this one!</translation>
+        <translation type="unfinished">We&apos;re working on this one!</translation>
     </message>
 </context>
-
 <context>
     <name>MarketplaceTest</name>
     <message>
@@ -73,158 +133,248 @@ Creator: %2
         <translation type="obsolete">Page under construction</translation>
     </message>
     <message>
+        <location filename="../ui/pages/MarketplaceTest.qml" line="90"/>
+        <source>Still working on this!!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pages/MarketplaceTest.qml" line="110"/>
         <source>Loading marketplace items...</source>
-        <translation type="obsolete">Loading marketplace items...</translation>
+        <translation type="unfinished">Loading marketplace items...</translation>
     </message>
     <message>
+        <location filename="../ui/pages/MarketplaceTest.qml" line="126"/>
         <source>Failed to load marketplace</source>
-        <translation type="obsolete">Failed to load marketplace</translation>
+        <translation type="unfinished">Failed to load marketplace</translation>
     </message>
     <message>
+        <location filename="../ui/pages/MarketplaceTest.qml" line="139"/>
         <source>Retry</source>
-        <translation type="obsolete">Retry</translation>
+        <translation type="unfinished">Retry</translation>
     </message>
     <message>
+        <location filename="../ui/pages/MarketplaceTest.qml" line="160"/>
         <source>Store</source>
-        <translation type="obsolete">Store</translation>
+        <translation type="unfinished">Store</translation>
     </message>
     <message>
+        <location filename="../ui/pages/MarketplaceTest.qml" line="161"/>
         <source>Downloaded</source>
-        <translation type="obsolete">Downloaded</translation>
+        <translation type="unfinished">Downloaded</translation>
     </message>
 </context>
 <context>
     <name>Mods</name>
     <message>
+        <location filename="../ui/pages/Mods.qml" line="22"/>
+        <source>Trying to set the font...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pages/Mods.qml" line="30"/>
+        <source>Success</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pages/Mods.qml" line="31"/>
+        <source>applied font</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pages/Mods.qml" line="42"/>
+        <source>Something went wrong</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pages/Mods.qml" line="43"/>
+        <source>Check the logs for more info.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pages/Mods.qml" line="73"/>
         <source>Open Mods Folder</source>
-        <translation type="obsolete">Open Mods Folder</translation>
+        <translation type="unfinished">Open Mods Folder</translation>
     </message>
     <message>
+        <location filename="../ui/pages/Mods.qml" line="75"/>
         <source>Open the local folder where mods are stored</source>
-        <translation type="obsolete">Open the local folder where mods are stored</translation>
+        <translation type="unfinished">Open the local folder where mods are stored</translation>
     </message>
     <message>
+        <location filename="../ui/pages/Mods.qml" line="80"/>
         <source>See how to use mods</source>
-        <translation type="obsolete">See how to use mods</translation>
+        <translation type="unfinished">See how to use mods</translation>
     </message>
     <message>
+        <location filename="../ui/pages/Mods.qml" line="81"/>
         <source>Open the bloxstrap&apos;s document.</source>
-        <translation type="obsolete">Open the bloxstrap&apos;s document.</translation>
+        <translation type="unfinished">Open the bloxstrap&apos;s document.</translation>
     </message>
     <message>
+        <location filename="../ui/pages/Mods.qml" line="90"/>
         <source>Custom Cursor</source>
-        <translation type="obsolete">Custom Cursor</translation>
+        <translation type="unfinished">Custom Cursor</translation>
     </message>
     <message>
+        <location filename="../ui/pages/Mods.qml" line="91"/>
         <source>Change cursor to old/new</source>
-        <translation type="obsolete">Change cursor to old/new</translation>
+        <translation type="unfinished">Change cursor to old/new</translation>
     </message>
     <message>
+        <location filename="../ui/pages/Mods.qml" line="100"/>
         <source>Custom Font</source>
-        <translation type="obsolete">Custom Font</translation>
+        <translation type="unfinished">Custom Font</translation>
     </message>
     <message>
+        <location filename="../ui/pages/Mods.qml" line="101"/>
         <source>Set your font to your liking!</source>
-        <translation type="obsolete">Set your font to your liking!</translation>
+        <translation type="unfinished">Set your font to your liking!</translation>
     </message>
     <message>
+        <location filename="../ui/pages/Mods.qml" line="112"/>
         <source>Select a File</source>
-        <translation type="obsolete">Select a File</translation>
+        <translation type="unfinished">Select a File</translation>
     </message>
 </context>
 <context>
     <name>Settings</name>
     <message>
+        <location filename="../ui/pages/Settings.qml" line="7"/>
         <source>Settings</source>
-        <translation type="obsolete">Settings</translation>
+        <translation type="unfinished">Settings</translation>
     </message>
     <message>
+        <location filename="../ui/pages/Settings.qml" line="14"/>
         <source>Appearance</source>
-        <translation type="obsolete">Appearance</translation>
+        <translation type="unfinished">Appearance</translation>
     </message>
     <message>
+        <location filename="../ui/pages/Settings.qml" line="19"/>
         <source>App Theme</source>
-        <translation type="obsolete">App Theme</translation>
+        <translation type="unfinished">App Theme</translation>
     </message>
     <message>
+        <location filename="../ui/pages/Settings.qml" line="20"/>
         <source>Select which app theme to display</source>
-        <translation type="obsolete">Select which app theme to display</translation>
+        <translation type="unfinished">Select which app theme to display</translation>
     </message>
     <message>
+        <location filename="../ui/pages/Settings.qml" line="26"/>
         <source>Light</source>
-        <translation type="obsolete">Light</translation>
+        <translation type="unfinished">Light</translation>
     </message>
     <message>
+        <location filename="../ui/pages/Settings.qml" line="27"/>
         <source>Dark</source>
-        <translation type="obsolete">Dark</translation>
+        <translation type="unfinished">Dark</translation>
     </message>
     <message>
+        <location filename="../ui/pages/Settings.qml" line="38"/>
         <source>Window Backdrop Effect</source>
-        <translation type="obsolete">Window Backdrop Effect</translation>
+        <translation type="unfinished">Window Backdrop Effect</translation>
     </message>
     <message>
+        <location filename="../ui/pages/Settings.qml" line="39"/>
         <source>Adjust the appearance of the window background (Only available on Windows platform, some styles may only support on Windows 11)</source>
-        <translation type="obsolete">Adjust the appearance of the window background (Only available on Windows platform, some styles may only support on Windows 11)</translation>
+        <translation type="unfinished">Adjust the appearance of the window background (Only available on Windows platform, some styles may only support on Windows 11)</translation>
     </message>
     <message>
+        <location filename="../ui/pages/Settings.qml" line="45"/>
         <source>Mica</source>
-        <translation type="obsolete">Mica</translation>
+        <translation type="unfinished">Mica</translation>
     </message>
     <message>
+        <location filename="../ui/pages/Settings.qml" line="46"/>
         <source>Acrylic</source>
-        <translation type="obsolete">Acrylic</translation>
+        <translation type="unfinished">Acrylic</translation>
     </message>
     <message>
+        <location filename="../ui/pages/Settings.qml" line="47"/>
         <source>Tabbed</source>
-        <translation type="obsolete">Tabbed</translation>
+        <translation type="unfinished">Tabbed</translation>
     </message>
     <message>
+        <location filename="../ui/pages/Settings.qml" line="48"/>
         <source>None</source>
-        <translation type="obsolete">None</translation>
+        <translation type="unfinished">None</translation>
     </message>
     <message>
+        <location filename="../ui/pages/Settings.qml" line="63"/>
         <source>Language</source>
-        <translation type="obsolete">Language</translation>
+        <translation type="unfinished">Language</translation>
     </message>
     <message>
+        <location filename="../ui/pages/Settings.qml" line="68"/>
         <source>Display Language</source>
-        <translation type="obsolete">Display Language</translation>
+        <translation type="unfinished">Display Language</translation>
     </message>
     <message>
+        <location filename="../ui/pages/Settings.qml" line="69"/>
         <source>Set your preferred language for Lution</source>
-        <translation type="obsolete">Set your preferred language for Lution</translation>
+        <translation type="unfinished">Set your preferred language for Lution</translation>
     </message>
     <message>
+        <location filename="../ui/pages/Settings.qml" line="76"/>
         <source>Use System Language</source>
-        <translation type="obsolete">Use System Language</translation>
+        <translation type="unfinished">Use System Language</translation>
     </message>
     <message>
+        <location filename="../ui/pages/Settings.qml" line="99"/>
         <source>About</source>
-        <translation type="obsolete">About</translation>
+        <translation type="unfinished">About</translation>
     </message>
     <message>
+        <location filename="../ui/pages/Settings.qml" line="107"/>
         <source>Lution Chroma</source>
-        <translation type="obsolete">Lution Chroma</translation>
+        <translation type="unfinished">Lution Chroma</translation>
     </message>
     <message>
+        <location filename="../ui/pages/Settings.qml" line="108"/>
         <source>Placeholder</source>
-        <translation type="obsolete">Placeholder</translation>
+        <translation type="unfinished">Placeholder</translation>
     </message>
     <message>
+        <location filename="../ui/pages/Settings.qml" line="118"/>
         <source>Repo</source>
-        <translation type="obsolete">Repo</translation>
+        <translation type="unfinished">Repo</translation>
     </message>
     <message>
+        <location filename="../ui/pages/Settings.qml" line="135"/>
         <source>License</source>
-        <translation type="obsolete">License</translation>
+        <translation type="unfinished">License</translation>
     </message>
     <message>
+        <location filename="../ui/pages/Settings.qml" line="136"/>
         <source>This project is licensed under the MIT license</source>
-        <translation type="obsolete">This project is licensed under the MIT license</translation>
+        <translation type="unfinished">This project is licensed under the MIT license</translation>
     </message>
     <message>
+        <location filename="../ui/pages/Settings.qml" line="139"/>
         <source>MIT License</source>
-        <translation type="obsolete">MIT License</translation>
+        <translation type="unfinished">MIT License</translation>
+    </message>
+</context>
+<context>
+    <name>splash</name>
+    <message>
+        <location filename="../ui/splash.qml" line="61"/>
+        <source>Launch Sober</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/splash.qml" line="62"/>
+        <source>Play roblox via sober with Chroma running in the backround.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/splash.qml" line="71"/>
+        <source>Launch Chroma</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/splash.qml" line="72"/>
+        <source>Launch Chroma and start customize stuff!</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
