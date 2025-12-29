5b5d6cf9aa1c7d9cb951f553e3a500be99c87303
evil chipo <itsjustchipoverhere@proton.me>
2025-12-29 13:28:21 +0700

    fix + remove unused imports

--- src/main.py
+++ src/main.py
@@ -1,33 +1,24 @@
-import subprocess
 import sys
-import webbrowser
 from pathlib import Path
-from time import sleep
 from tkinter.constants import NONE
 
-import darkdetect
 from PySide6.QtCore import (
     QLocale,
-    QObject,
-    QThread,
     QTranslator,
-    Signal,
-    Slot,
 )
-from PySide6.QtGui import QAction, QGuiApplication, QIcon
-from PySide6.QtWidgets import QApplication, QMenu, QSystemTrayIcon
+from PySide6.QtWidgets import QApplication
 
-from modules.backend import Backend
 from modules.config import Config
 from modules.config.config_init import initConfig
 from modules.launchmenu import LaunchMenu
 from RinUI import RinUITranslator, RinUIWindow
 
+initConfig()  # this first
+from modules.backend import Backend
+
 SCRIPT_DIR = Path(__file__).parent
 
-
-cfg = Config(Path("LutionConfig.toml"))
-initConfig()
+cfg = Config()
 __version__ = "0.1.0"
 
 print(f"""
