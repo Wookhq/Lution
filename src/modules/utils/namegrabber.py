45e2ef3eda4335b381eb81f1cf2350f4e3a68e71
ekard <105611677+helloplauz10@users.noreply.github.com>
2026-01-11 18:23:17 +0800

    rely on logs instead of cookies

--- src/modules/utils/namegrabber.py
+++ src/modules/utils/namegrabber.py
@@ -3,36 +3,67 @@
 
 import json
 import os
+import re
 
 import requests
 
+from modules.config import Config # to chip: just why
 
-def getCookie():  # trust
-    from modules.config import Config
+cfg = Config()
 
-    cfg = Config()
+# def getCookie():  # trust
+#     sb = cfg.get_row("Sober", "Path")
+#     cookies = os.path.expanduser(f"{sb}/data/sober/cookies")  # get the cookie path
 
+#     with open(cookies, "r") as c:  # open it
+#         data = c.read()  # read it
+#         c.close()
+#         cookies = data.split(";")  # dict it
+#         cookie_dict = {}
+
+#         for c in cookies:
+#             if "=" in c:
+#                 k, v = c.strip().split("=", 1)
+#                 cookie_dict[k] = v
+
+#     return cookie_dict  # then return the dict
+
+def getUserIdFromLogs():
     sb = cfg.get_row("Sober", "Path")
-    cookies = os.path.expanduser(f"{sb}/data/sober/cookies")  # get the cookie path
+    logsPath = os.path.expanduser(f"{sb}/data/sober/sober_logs")
+    logFiles = sorted(
+        [os.path.join(logsPath, i) for i in os.listdir(logsPath)],
+        key = os.path.getmtime,
+        reverse = True
+    )
+    for logFile in logFiles:
+        with open(logFile) as logFileIO:
+            print(f"Searching {logFile} for user id")
+            for line in logFileIO:
+                if "[FLog::GameJoinLoadTime] Report game_join_loadtime:" in line:
+                    userid = re.search(
+                        r"userid:(\d+)",
+                        line
+                    )
+                    if not userid:
+                        print(f"Couldn't find user id from this line: {line}")
+                    userid = int(userid.group(1))
+                    print(f"Retrieved user id: {userid}")
+                    return userid
+        print(f"{logFile} doesn't contain any logs related to joining a game. Searching next log file")
+    print(f"Couldn't find user id, returning None!")
+    return
 
-    with open(cookies, "r") as c:  # open it
-        data = c.read()  # read it
-        c.close()
-        cookies = data.split(";")  # dict it
-        cookie_dict = {}
+# def getName(cookies: dict):  # trust the process
+#     r = requests.get(
+#         "https://users.roblox.com/v1/users/authenticated", cookies=cookies
+#     )  # fetching roblox's offical api for display name
+#     r.raise_for_status()
+#     data = r.json()
+#     return data.get("displayName", "User")  # return name
 
-        for c in cookies:
-            if "=" in c:
-                k, v = c.strip().split("=", 1)
-                cookie_dict[k] = v
-
-    return cookie_dict  # then return the dict
-
-
-def getName(cookies: dict):  # trust the process
-    r = requests.get(
-        "https://users.roblox.com/v1/users/authenticated", cookies=cookies
-    )  # fetching roblox's offical api for display name
+def getName(userId: int):
+    r = requests.get(f"https://users.roblox.com/v1/users/{userId}")
     r.raise_for_status()
     data = r.json()
-    return data.get("displayName", "User")  # return name
+    return data.get("displayName", "User")
