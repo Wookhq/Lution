af542774ce95685791c1b6c08041d397c3d98490
chippy <itsjustchipoverhere@proton.me>
2026-01-16 14:48:36 +0700

    we do a lil bit of testing

--- src/test.py
+++ src/test.py
@@ -1,8 +1,12 @@
 from modules.config.config_init import initConfig
+
 initConfig()
-from modules.marketplace import MarketplaceHelper
 import json
+
+from modules.marketplace import MarketplaceHelper
 
 mk = MarketplaceHelper()
 
 print(json.dumps(mk.list_items(), indent=4))
+
+mk.download_item("c9007043-ca0b-4e7e-8874-2ea7a3187416")
