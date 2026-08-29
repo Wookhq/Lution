f7141d08f76edf2c12878a275db922f45419f7fb
ang <231962178+anamelessguy1@users.noreply.github.com>
2026-08-29 14:47:47 +0100

    Create run.sh

---
+++ src/run.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+echo "you are following lution's old guide"
+echo "Lution has been rewritten and is now a simple executable instead of a script"
+echo "go to https://github.com/Wookhq/Lution/releases/ and download the latest version"
